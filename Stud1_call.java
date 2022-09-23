565636cf0c7e8d898137ccf42b17668ce636bd99
unknown <shru084@gmail.com>
2022-09-23 20:13:16 +0000

    Get student details based on id

---
+++ Stud1_call.java
@@ -0,0 +1,41 @@
+package jdbc_maven;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Stud1_call {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcprc","root","root");
+			CallableStatement cs=con.prepareCall("call get1_student(?)");
+			System.out.println("enter the id");
+			int i=sc.nextInt();
+			cs.setInt(1,i);
+		ResultSet rs=cs.executeQuery();
+		while(rs.next()) {
+			System.out.println(rs.getInt(1));
+			System.out.println(rs.getString(2));
+			System.out.println(rs.getLong(3));
+			System.out.println(rs.getString(4));
+			System.out.println(rs.getInt(5));
+		}
+			System.out.println("Student details");
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
